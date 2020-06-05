5b96a9426817627b6bc0e98b1643a6fa31569be6
Daniel Caldera <bdcaldera@bdcaldera.com>
2020-06-05 03:52:21 -0500

    added a lot of things je

--- fans-app/src/index.js
+++ fans-app/src/index.js
@@ -1,21 +1,48 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
-import { Provider } from "react-redux"
-import { store } from "./Redux/index"
-import ShowAllMovies from './Components/ShowAllMovies/index.js'
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './assets/styles/index.scss'
+import App from './routes/App'
+import * as serviceWorker from './utils/serviceWorker'
+import ApolloClient from 'apollo-boost'
+import { ApolloProvider } from 'react-apollo'
 
+const serverUri =
+    process.env.NODE_ENV === 'development' ? 'http://localhost:3500/graphql' : 'https://team-50-tomatoes-server.now.sh/'
+
+const client = new ApolloClient({
+    uri: serverUri,
+    fetchOptions: {
+        credentials: 'include',
+    },
+    request: (operation) => {
+        const token = localStorage.getItem('token')
+        operation.setContext({
+            headers: {
+                authorization: token,
+            },
+        })
+    },
+    onError: ({ networkError }) => {
+        if (networkError) {
+            console.log('Network Error', networkError)
+
+            // if (networkError.statusCode === 401) {
+            //     localStorage.removeItem('token')
+            // }
+        }
+    },
+})
 
 ReactDOM.render(
-  <Provider store={store}>
-    <ShowAllMovies />
-  </Provider>,
-  document.getElementById('root')
-);
+    <React.StrictMode>
+        <ApolloProvider client={client}>
+            <App />
+        </ApolloProvider>
+    </React.StrictMode>,
+    document.getElementById('root')
+)
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+serviceWorker.unregister()
