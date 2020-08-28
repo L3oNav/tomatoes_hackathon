e07ee5d76cd91c05331809cccbdb8ee6a6125f49
Leonardo Nava (L3oNav) <L3oNav@outlook.com>
2020-08-27 18:18:57 -0700

    video camera added

--- fans-app/src/Components/Video/index.js
+++ fans-app/src/Components/Video/index.js
@@ -2,7 +2,8 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
 import { moviesVideosAction } from '../../Redux/Actions/video';
-
+import P5Wrapper from 'react-p5-wrapper'
+import objectDetectionSketch from '../ObjectDetectionSketch'
 class Video extends Component {
 	async componentDidMount() {
 		await this.props.moviesVideosAction(this.props.id);
@@ -12,6 +13,7 @@ class Video extends Component {
 		if (this.props.video.video.results) {
 			return (
 				<div>
+					<P5Wrapper sketch={objectDetectionSketch}/>
 					<iframe
 						width="640"
 						height="480"
